ae26540e4c11a8c45ce4f449ce4b836d4fef998e
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-08-29 12:10:24 +0100

    refactor: move Circle collision methods to geometry.cpp

--- src/application.cpp
+++ src/application.cpp
@@ -178,16 +178,5 @@ namespace Hooray {
 
     }
 
-    bool Circle::contains(const Vector2 other) const {
-        return ::CheckCollisionPointCircle(ToRay(other), ToRay(this->center), this->radius);
-    }
-
-    bool Circle::overlaps(const Circle other) const {
-        return ::CheckCollisionCircles(ToRay(this->center), this->radius, ToRay(other.center), other.radius);
-    }
-
-    bool Circle::overlaps(const Rectangle other) const {
-        return ::CheckCollisionCircleRec(ToRay(this->center), this->radius, ToRay(other));
-    }
 
 }
---
+++ src/geometry.cpp
@@ -0,0 +1,18 @@
+#include "conversion.hpp"
+#include "types.hpp"
+
+namespace Hooray {
+
+    bool Circle::contains(const Vector2 other) const {
+        return ::CheckCollisionPointCircle(ToRay(other), ToRay(this->center), this->radius);
+    }
+
+    bool Circle::overlaps(const Circle other) const {
+        return ::CheckCollisionCircles(ToRay(this->center), this->radius, ToRay(other.center), other.radius);
+    }
+
+    bool Circle::overlaps(const Rectangle other) const {
+        return ::CheckCollisionCircleRec(ToRay(this->center), this->radius, ToRay(other));
+    }
+
+}
